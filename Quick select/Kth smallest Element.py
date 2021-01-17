a35bda913f6574861125b8aa0539d663a134e165
srisrinu1 <srinivasreddychalla133@gmail.com>
2021-01-17 18:17:58 +0530

    srisrinu1 / Algorithms-useful-for-Interviews

---
+++ Quick select/Kth smallest Element.py
@@ -0,0 +1,8 @@
+from random import randint
+def QuickSelect(left,right,K):
+def main():
+    lst=[]
+    for _ in range(10):
+        lst.append(randint(1,100)) 
+    K=int(input())    
+    print(QuickSelect(0,len(lst)-1),K)       
